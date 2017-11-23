7c7147385bbc9cbdb51abfc04ad5e25a4f970853
Brad P <b.powles@warwickschool.org>
2017-11-23 20:58:55 +0000

    Seperated out functions and collated main() call.

--- networking.py
+++ networking.py
@@ -1,10 +1,10 @@
-from sys import version_info
 import subprocess
-import time
 import re
 import os
+
 os.system("cls")
 os.system("color a")
+
 proc = str(subprocess.Popen(["ipconfig","/all"], stdout=subprocess.PIPE, shell=True).communicate())
 ipv4 = (str(re.findall("(?<=IPv4 Address. . . . . . . . . . . : )(\d+\.\d+\.\d+\.\d+)",proc)))
 dhcp = (str(re.findall("(?<=DHCP Server . . . . . . . . . . . : )(\d+\.\d+\.\d+\.\d+)",proc)))
@@ -21,12 +21,46 @@ def menu():
     print("\n         e - Exit Program")
     print("\n")
     print("************************************************************")
+
+
 def takeInput(message):
     try:
         reply = raw_input(message)
     except:
         reply = input(message)
     return reply
+
+def ping():
+    opt = takeInput("Address or exit (e)> ")
+    if opt == "e" or opt == "exit":
+        pass
+    else:
+        print("\nInput how much time in ms until the ping times out")
+        print("Tip for average ping times in ms - Good ping:<5 | Medium ping:30 | Bad ping:>100")
+        opt2 = input("Time? (0-255 ms) or exit (e)> ")
+        if opt2 == "e" or opt2 == "exit":
+            pass
+        else:
+            print("\nInput how many times the address will be pinged")
+            print("Tip for average tries - If tries have to go above 4, there is usually a network issue. ")
+            opt3 = input("Tries? (0-1000) or exit (e)> ")
+            if opt3 == "e" or opt3 == "exit":
+                pass
+            else:
+                full = "ping " + opt + " -i " + opt2 + " -n " + opt3
+                os.system(full)
+
+def networkInfo():
+    print("Networking Information:\n\n")
+    print("Default Gateway: ",dhcp)
+    print("Network DHCP server: ",dhcp)
+    print("Current subnet mask: ",submask)
+    print("Current IPV4 Address: ",ipv4)
+
+
+
+
+
 def main():
     menu()
     opt = takeInput("> ")
@@ -34,54 +68,29 @@ def main():
         print("\n\nProgram closing")
         os.system("color 7")
         os.system("cls")
+        exit()
     elif opt == "1":
         opt = input("Command> ")
         os.system(opt)
-        main()
     elif opt == "2":
         os.system("ipconfig /all")
-        main()
     elif opt == "3":
         opt2 = input("Address or exit (e)> ")
         if opt2 == "e" or opt2 == "exit":
-            main()
+            pass
         else:
             full = "tracert "+opt2
             os.system(full)
-            main()
     elif opt == "4":
-        opt2 = input("Address or exit (e)> ")
-        if opt2 == "e" or opt2 == "exit":
-            main()
-        else:
-            print("\nInput how much time in ms until the ping times out")
-            print("Tip for average ping times in ms - Good ping:<5 | Medium ping:30 | Bad ping:>100")
-            opt3 = input("Time? (0-255 ms) or exit (e)> ")
-            if opt3 == "e" or opt3 == "exit":
-                main()
-            else:
-                print("\nInput how many times the address will be pinged")
-                print("Tip for average tries - If tries have to go above 4, there is usually a network issue. ")
-                opt4 = input("Tries? (0-1000) or exit (e)> ")
-                if opt4 == "e" or opt4 == "exit":
-                    main()
-                else:
-                    full = "ping "+opt2+" -i "+opt3+" -n "+opt4
-                    os.system(full)
-                    main()
+        ping()
     elif opt == "5":
-        print("Networking Information:\n\n")
-        print("Default Gateway: ",dhcp)
-        print("Network DHCP server: ",dhcp)
-        print("Current subnet mask: ",submask)
-        print("Current IPV4 Address: ",ipv4)
-        main()
+        networkInfo()
     elif opt == "6":
         os.system("arp -a")
-        main()
     else:
         print("Invalid option...")
-        main()
+    main()
 
 
-main()
+if __name__ == "__main__":
+    main()
